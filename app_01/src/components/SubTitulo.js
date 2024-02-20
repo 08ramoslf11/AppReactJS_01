678882681ae2b98eeb9e4e54a1948a18cf669c5c
08ramoslf11 <157762398+08ramoslf11@users.noreply.github.com>
2024-02-19 21:52:07 -0300

    primeiros componentes simples

---
+++ app_01/src/components/SubTitulo.js
@@ -0,0 +1,5 @@
+export default function SubTitulo(props){
+    return(
+        <h3>{props.Texto}</h3>
+    )
+}
---
+++ app_01/src/components/Titulo.js
@@ -0,0 +1,5 @@
+export default function Titulo(){
+    return (
+        <h1>Olá mundo !!!</h1>
+    )
+}
